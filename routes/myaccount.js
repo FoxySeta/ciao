10b2166ea6488132d5bf53a08506f6525c92d35a
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-10-04 17:30:22 +0200

    Friends (db + list)

--- routes/myaccount.js
+++ routes/myaccount.js
@@ -5,16 +5,21 @@ const router = require('express').Router();
 router.get('/',
   require('connect-ensure-login').ensureLoggedIn(),
   (req, res, next) => {
-    require('../db').get('SELECT rowid AS id, email, name, qrcode FROM users WHERE rowid = ?', [req.user.id], function (err, row) {
+    require('../db').get('SELECT rowid AS id, email, name, qrcode FROM Users WHERE rowid = ?', [req.user.id], (err, row) => {
       if (err)
         return next(err);
-      res.render('profile', {
-        user: {
-          id: row.id.toString(),
-          username: row.email,
-          name: row.name,
-          qrcode: row.qrcode
-        }
+      require('../db').get('SELECT name, email FROM Users JOIN Friendships ON email = Friendships.user_email1 WHERE Friendships.user_email2 = ?', [req.user.email], (err, rows) => {
+        if (err)
+          return next(err);
+        res.render('profile', {
+          user: {
+            id: row.id.toString(),
+            username: row.email,
+            name: row.name,
+            qrcode: row.qrcode,
+            friends: [{name: 'Mickey Mouse', email: 'mickey.mouse@disney.com'}]
+          }
+        });
       });
     });
   });
