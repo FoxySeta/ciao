67dd3d31ce49ce85bc0efb49545417ea0636414b
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-10-04 23:51:57 +0200

    Webpage setup

--- db.js
+++
@@ -1,8 +0,0 @@
-// db.js: database access
-
-const Database = require('sqlite3').Database;
-
-module.exports = new Database('database.sqlite3', err => {
-  if (err)
-    console.error(err.message);
-});
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-// routes/index.js: main route
-
-const router = require('express').Router();
-
-router.get('/', (req, res, _) => {
-  res.render('index', {user: req.user});
-});
-
-module.exports = router;
